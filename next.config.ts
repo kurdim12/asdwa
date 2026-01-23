324be3b61e0d27e5cd60d6b3b525dc804e75c02a
kurdim12 <abdalrhmankurdi12@gmail.com>
2026-01-24 01:50:18 +0300

    Fix invalid next.config.ts: remove turbopack config

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // @ts-ignore
-    turbopack: {
-      root: process.cwd(),
-    }
-  }
 };
 
 export default nextConfig;
